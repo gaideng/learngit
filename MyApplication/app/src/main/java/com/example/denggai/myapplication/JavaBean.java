9840f98841f20ab4fe413790f2510d4f63dab4ec
邓概 <18570630788@163.com>
2016-09-27 22:53:38 +0800

    File MyApp

---
+++ MyApplication/app/src/main/java/com/example/denggai/myapplication/JavaBean.java
@@ -0,0 +1,38 @@
+package com.example.denggai.myapplication;
+
+import java.util.List;
+
+/**
+ * Created by denggai on 2016/8/2.
+ */
+public class JavaBean {
+
+
+    private List<String> 上海;
+    private List<String> 四川;
+    private List<String> 福建;
+
+    public List<String> get上海() {
+        return 上海;
+    }
+
+    public void set上海(List<String> 上海) {
+        this.上海 = 上海;
+    }
+
+    public List<String> get四川() {
+        return 四川;
+    }
+
+    public void set四川(List<String> 四川) {
+        this.四川 = 四川;
+    }
+
+    public List<String> get福建() {
+        return 福建;
+    }
+
+    public void set福建(List<String> 福建) {
+        this.福建 = 福建;
+    }
+}
